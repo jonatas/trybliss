b439122e3a90570e4c2dddeb057881edc4105e91
Jônatas Paganini <jonatasdp@gmail.com>
2013-05-16 01:10:01 -0300

    new images and map to keep it working. exchanging format to create new knowledges to be more easy to write

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 if (Meteor.isClient) {
   Meteor.startup(function(){
-  if (!Session.get('currentLevel'))
-    Session.set('currentLevel',0);
+    if (!Session.get('currentLevel')) Session.set('currentLevel',0);
+  });
+  Meteor.autorun(function(){
+    if (! window.linkSymbols) {
+      data = $.ajax({ url: "/images/link_symbols.txt", async: false, }).responseText;
+      window.linkSymbols = {};
+      _.each(data.split("\n"),function(line){
+         parts = line.split(" ");
+         window.linkSymbols[parts[0]] = parts[1];
+       });
+    }
   });
 
   function notify(type,message){
@@ -9,6 +18,7 @@ if (Meteor.isClient) {
   }
   Template.game.levels = function() {
     return [
+      /*
       {
         title: "Places",
         learn: { symbols: [ 'house', 'money', 'god', 'feeling','medical'] },
@@ -40,46 +50,78 @@ if (Meteor.isClient) {
         ],
         question: { example: 'house',  plus: 'car', equal: 'garage'},
         wrong_alternatives: ['bank', 'god', 'restaurant']
-      },
+      },*/
       {
         title: "Knowledge",
-        learn: { symbols: [ 'knowledge', 'mind', "exchange-v", "up"] },
-        help: [
-         { example: 'knowledge', plus: 'learn', equal:'explain-v'}
-        ],
-        question: { example: 'house',  plus: 'learn', equal: 'school'},
-        wrong_alternatives: ['known', 'hospital', 'restaurant']
+        learn: {
+          symbols: [ 'knowledge', 'mind', "up"],
+          combinations: [ 'knowledge and learn-(to) equal teach']
+        },
+        answer: {
+          question: 'house and learn-(to) equal question',
+          answer: 'school',
+          alternatives: ['hospital', 'home', 'school','airport']
+        }
+      },
+      {
+        title: "Animals",
+        learn: {
+          symbols: [ 'animal', 'sea', 'container'],
+          combinations: ['animal and water and container equal pelican',
+                         'animal and water equal duck_']
+        },
+        answer: {
+          question: 'animal and water equal question',
+          answer: 'duck',
+          alternatives: ['pig','duck','melon','snake']
+        }
       }
    ];
   };
+
+  window.symbolPath = function(symbol){
+        if (window.linkSymbols!=null)
+          return "/images/"+(window.linkSymbols[symbol] || symbol)+".png";
+        else
+          return "/images/"+symbol+".png";
+  }
+  Template.show_symbol.helpers({
+    src: function () { return symbolPath(this.symbol) }
+  });
   Template.game.level = function() {
     return Template.game.levels()[Session.get("currentLevel")];
+  }
+  Template.combine.combinations = function() {
+    level = Template.game.level()
+    return _.map(level.learn.combinations, function(combination){return {combination: _.map(combination.split(" "),function(symbol){return {symbol:symbol};})};});
+  }
+  Template.game.symbols = function(){
+    return Template.game.level().learn.symbols;
   }
   Template.game.currentLevel = function(){
     return Session.get("currentLevel")+1;
   }
   Template.learn.symbols = function() {
     level = Template.game.level();
-    return _.map(level.learn.symbols, function(symbol){ return {symbol: symbol}; });
+    return _.map(level.learn.symbols, function(symbol){ return {symbol: symbol} });
   }
-  Template.do_question.alternatives = function() {
-    level = Template.game.level();
-    array = [{alternative: level.question.equal}];
-    for(i=0;i< level.wrong_alternatives.length;i++)
-      array.push({alternative: level.wrong_alternatives[i]});
-
-    return array;
+  Template.answer.question_fragments = function() {
+    return _.map(Template.game.level().answer.question.split(" "),function(symbol){return {symbol:symbol}});
+  }
+  Template.answer.alternatives = function() {
+    alternatives = Template.game.level().answer.alternatives;
+    return _.map(alternatives,function(alternative){return {alternative: alternative};});
   }
   Template.game.events({
     'click .alternative' : function (e) {
     level = Template.game.levels()[Session.get('currentLevel')];
      $(".answer").removeClass("btn-success");
      $(".answer").removeClass("btn-danger");
-     right = (this.alternative == level.question.equal);
+     right = (this.alternative == level.answer.answer);
      if (!right)
        $(".answer").text(":(" );
 
-     $(e.target.parentElement).find("img").attr("src", "/images/"+this.alternative+".jpg");
+     $(e.target.parentElement).find("img").attr("src", symbolPath(this.alternative));
      $(e.target).removeClass("btn btn-action").addClass(right ? "btn-success" : "btn-danger");
      $(".alert").remove();
 
@@ -90,7 +132,6 @@ if (Meteor.isClient) {
        } else
          notify("success", "congrats! you finished up!");
      }else notify("danger", "Try again!")
-     
     }
   });
 

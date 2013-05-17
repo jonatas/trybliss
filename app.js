3011a8de4be573850072a21715e403be8a9f4f91
Jônatas Paganini <jonatasdp@gmail.com>
2013-05-17 00:32:42 -0300

    start job to put user levels too. storing it in Levels collection.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+Levels = new Meteor.Collection('levels');
+
 if (Meteor.isClient) {
   Meteor.startup(function(){
     if (!Session.get('currentLevel')) Session.set('currentLevel',0);
@@ -11,15 +13,78 @@ if (Meteor.isClient) {
          window.linkSymbols[parts[0]] = parts[1];
        });
     }
+    Meteor.subscribe("levels");
   });
   Template.game.rendered = function() {
-  $('#learning-steps a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
+    $('#learning-steps a').click(function (e) {
+      e.preventDefault();
+      $(this).tab('show');
+    });
   };
   Template.game.levels = function() {
-    return [
+    return Levels.find().fetch();
+  };
+
+  window.symbolPath = function(symbol){
+        if (window.linkSymbols!=null)
+          return "/images/"+(window.linkSymbols[symbol] || symbol)+".png";
+        else
+          return "/images/"+symbol+".png";
+  }
+  Template.show_symbol.helpers({
+    src: function () { return symbolPath(this.symbol) }
+  });
+  Template.game.level = function() {
+    return Template.game.levels()[Session.get("currentLevel")];
+  }
+  Template.combine.combinations = function() {
+    if (level = Template.game.level())
+      return _.map(level.learn.combinations, function(combination){return {combination: _.map(combination.split(" "),function(symbol){return {symbol:symbol};})};});
+  }
+  Template.game.symbols = function(){
+    return Template.game.level().learn.symbols;
+  }
+  Template.game.currentLevel = function(){
+    return Session.get("currentLevel")+1;
+  }
+  Template.learn.symbols = function() {
+    if (level = Template.game.level())
+      return _.map(level.learn.symbols, function(symbol){ return {symbol: symbol} });
+  }
+  Template.answer.question_fragments = function() {
+    if (level=Template.game.level())
+      return _.map(level.answer.question.split(" "),function(symbol){return {symbol:symbol}});
+  }
+  Template.answer.alternatives = function() {
+    if (level=Template.game.level())
+      return _.map(level.answer.alternatives,function(alternative){return {alternative: alternative};});
+  }
+  Template.game.events({
+    'click .alternative' : function (e) {
+    level = Template.game.level();
+    console.log(e,this);
+     $("#answer").removeClass("alert-success");
+     $("#answer").removeClass("alert-error");
+     right = (this.alternative == level.answer.answer);
+
+     $(e.target.parentElement).find("img").attr("src", symbolPath(this.alternative));
+     $(e.target).removeClass("btn btn-action").addClass(right ? "btn-success" : "btn-danger");
+
+     if (right){
+       if (Session.get("currentLevel") < Template.game.levels().length-1) {
+         $("#answer").addClass("alert-info");
+         _.delay(function(level) { Session.set("currentLevel",level); }, 3000, Session.get("currentLevel")+1);
+       } else {
+         $("#answer").addClass("alert-success");
+       }
+     }else $("#answer").addClass("alert-error");
+    }
+  });
+}
+if (Meteor.isServer) {
+  Meteor.startup(function(){
+  if (Levels.find().count() == 0) {
+    var levels = [
       {
         title: "Places",
         learn: {
@@ -105,65 +170,11 @@ if (Meteor.isClient) {
           alternatives: ['none','indicator_(present_action)','future']
         }
       }
-
    ];
-  };
-
-  window.symbolPath = function(symbol){
-        if (window.linkSymbols!=null)
-          return "/images/"+(window.linkSymbols[symbol] || symbol)+".png";
-        else
-          return "/images/"+symbol+".png";
+   _.each(levels, function(level){
+     Levels.insert(level);
+   });
   }
-  Template.show_symbol.helpers({
-    src: function () { return symbolPath(this.symbol) }
+   Meteor.publish("levels", function(){ return Levels.find() });
   });
-  Template.game.level = function() {
-    return Template.game.levels()[Session.get("currentLevel")];
-  }
-  Template.combine.combinations = function() {
-    if (level = Template.game.level())
-      return _.map(level.learn.combinations, function(combination){return {combination: _.map(combination.split(" "),function(symbol){return {symbol:symbol};})};});
-  }
-  Template.game.symbols = function(){
-    return Template.game.level().learn.symbols;
-  }
-  Template.game.currentLevel = function(){
-    return Session.get("currentLevel")+1;
-  }
-  Template.learn.symbols = function() {
-    if (level = Template.game.level())
-      return _.map(level.learn.symbols, function(symbol){ return {symbol: symbol} });
-  }
-  Template.answer.question_fragments = function() {
-    if (level=Template.game.level())
-      return _.map(level.answer.question.split(" "),function(symbol){return {symbol:symbol}});
-  }
-  Template.answer.alternatives = function() {
-    if (level=Template.game.level())
-      return _.map(level.answer.alternatives,function(alternative){return {alternative: alternative};});
-  }
-  Template.game.events({
-    'click .alternative' : function (e) {
-    level = Template.game.level();
-    console.log(e,this);
-     $("#answer").removeClass("alert-success");
-     $("#answer").removeClass("alert-error");
-     right = (this.alternative == level.answer.answer);
-
-     $(e.target.parentElement).find("img").attr("src", symbolPath(this.alternative));
-     $(e.target).removeClass("btn btn-action").addClass(right ? "btn-success" : "btn-danger");
-
-     if (right){
-       if (Session.get("currentLevel") < Template.game.levels().length-1) {
-         $("#answer").addClass("alert-info");
-         _.delay(function(level) { Session.set("currentLevel",level); }, 3000, Session.get("currentLevel")+1);
-       } else {
-         $("#answer").addClass("alert-success");
-       }
-     }else $("#answer").addClass("alert-error");
-    }
-  });
-}
-if (Meteor.isServer) {
 }

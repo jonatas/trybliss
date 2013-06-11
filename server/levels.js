32c68d3788a6b7146029cd9280c62792c7aeb244
Jônatas Davi Paganini <jonatasdp@gmail.com>
2013-06-11 09:44:32 -0300

    inserting level explain the tool #9

--- server/levels.js
+++ server/levels.js
@@ -1,96 +1,28 @@
 var levels = [
    {
-     title: "Places",
-     learn: {
-       symbols: [ 'house', 'money', 'God', 'feeling','medical'],
-       combinations : [
-        'house and money equal bank',
-        'house and God equal church',
-        'house and feeling equal home'
-       ]
-     },
-     answer: {
-       question: 'house and medical equal question',
-       answer: 'hospital',
-       alternatives: ['home', 'church', 'hospital', 'restaurant']
-     }
-   },
-   {
-     title: "More about places",
-     learn: {
-       symbols: [ 'public_room', 'food', 'medical'],
-       combinations: [
-         'house and medical equal hospital',
-         'container and money equal business',
-         'public_room and business equal store'
-       ]
-     },
-     answer: {
-         question: 'public_room and food equal question',
-         answer: 'restaurant',
-         alternatives: ['restaurant','home', 'church', 'hotel']
-     }
-   },
-   {
-     title: "More places and things",
-     learn: {
-       symbols: [ 'house', 'boat', 'car', 'person'],
-       combinations: [
-        'person and car equal driver',
-        'house and boat equal boathouse',
-        'boat and house equal houseboat'
-       ]
-     },
-     answer: {
-       question: 'house and car equal question',
-       answer: 'garage',
-       alternatives: ['bank', 'garage', 'restaurant']
-     }
-   },
-   {
-     title: "Knowledge",
-     learn: {
-       symbols: [ 'knowledge', 'give', 'get'],
-       combinations: [ 
-         'knowledge and give equal teach',
-         'knowledge and get equal learn-(to)'
-         ]
-     },
-     answer: {
-       question: 'house and learn-(to) equal question',
-       answer: 'school',
-       alternatives: ['hospital', 'home', 'school','airport']
-     }
-   },
-   {
-     title: "Fly",
-     learn: {
-       symbols: [ 'wing(s)', 'man', 'bird', 'wheel'],
-       combinations: ['wing(s) and man equal angel_(1)',
-                      'bird and water equal duck']
-     },
-     answer: {
-       question: 'wing(s) and wheel',
-       answer: 'airplane',
-       alternatives: ['duck','airplane','airport']
-     }
-   },
-   {
-     title: "Time",
-     learn: {
-       symbols: [ 'past', 'today', 'yesterday'],
-       combinations: ['day and indicator_(past_action) equal yesterday',
-                      'day and indicator_(future_action) equal tomorrow']
-     },
-     answer: {
-       question: 'indicator_(past_action) and indicator_(future_action) equal question',
-       answer: 'indicator_(present_action)',
-       alternatives: ['none','indicator_(present_action)','future']
-     }
+     content: "# Content Header"+
+       "\n* using [markdown syntax](http://daringfireball.net/projects/markdown/syntax)"+
+       "\n* vim keybidings (press i to start inserting)"+
+       "\n* when you change the code the preview will be changed"+
+       "\n* start typing and use ctrl-space to see the suggestions"+
+       "\n"+
+       "\n![archery][]"+
+       "\n"+
+       "\nArquery"+
+       "\n"+
+       "## Dont forget to use reference there."+
+       "\n"+
+       "\nI'm fixing these with a snipped, but for a while,"+
+       "\nyou need to reference each image here in the bottom."+
+       "\n"+
+       "\n[archery]: /images/symbols/archery.png"+
+       "\n"+
+       "\n---"
    }
 ];
 
 Meteor.startup(function(){
+   Levels.find().forEach(function(e){Levels.remove(e._id)});
   if (Levels.find().count() == 0) {
    Levels.find().forEach(function(e){Levels.remove(e._id)});
    _.each(levels, function(level){
